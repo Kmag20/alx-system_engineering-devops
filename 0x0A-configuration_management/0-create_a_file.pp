cf45767ea548703d982a9f199b73c4795078b0bd
Kmag20 <owenmawira@gmail.com>
2024-03-25 14:28:30 +0300

    Create a file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,11 +1,11 @@
 # create a file in the directory /tmp
 
 file { '/tmp/school':
-	ensure     => present,
-	mode       => '0744',
-	owner      => www-data,
-	group      => www-data,
-	path       => '/tmp/school',
-	content    => 'I love Puppet',
+  ensure  => present,
+  mode    => '0744',
+  owner   => www-data,
+  group   => www-data,
+  path    => '/tmp/school',
+  content => 'I love Puppet',
 }
 
