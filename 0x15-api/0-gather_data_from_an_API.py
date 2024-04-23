a962d33096fecbd506f00a1943034b0771bad9ff
Kmag20 <owenmawira@gmail.com>
2024-04-23 13:28:44 +0300

    Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,13 +6,22 @@ info about their TODO list progress
 import requests
 import sys
 
-if __name__ == '__main__':
-    user_id = sys.argv[1]
+if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
-    users = requests.get(url + "users/{}".format(user_id)).json()
-    params = {"userId": "{}".format(user_id)}
-    todos = requests.get(url + "todos", params=params).json()
-    completed_tasks = list(filter(lambda todo: todo['completed'], todos))
-    print("Employee {} is done with tasks ({}/{}):".format(users['name'],
-          len(completed_tasks), len(todos)))
-    [print("\t {}".format(task['title'])) for task in completed_tasks]
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+
+    completed = [t.get("title") for t in todos if t.get("completed") is True]
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(completed), len(todos)))
+    [print("\t {}".format(c)) for c in completed]
+
+#    user_id = sys.argv[1]
+#    url = "https://jsonplaceholder.typicode.com/"
+#    users = requests.get(url + "users/{}".format(user_id)).json()
+#    params = {"userId": "{}".format(user_id)}
+#    todos = requests.get(url + "todos", params=params).json()
+#    completed_tasks = list(filter(lambda todo: todo['completed'], todos))
+#    print("Employee {} is done with tasks ({}/{}):".format(users['name'],
+#          len(completed_tasks), len(todos)))
+#    [print("\t {}".format(task['title'])) for task in completed_tasks]
