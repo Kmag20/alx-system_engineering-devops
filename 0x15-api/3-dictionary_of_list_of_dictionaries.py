4e3f6d064eeee0499e72e8bee59ea69b759821ae
Kmag20 <owenmawira@gmail.com>
2024-04-25 00:39:37 +0300

    Dictionary of list of dictionaries

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""" Using what you did in the task #0, extend your Python script to export data in the JSON format.
+""" Using what you did in the task #0, extend your Python script
+to export data in the JSON format.
 """
 
 import json
@@ -12,11 +13,11 @@ if __name__ == '__main__':
     users = requests.get(url + "users").json()
 
     def todos(user_id):
-        params={'userId': user_id}
+        params = {'userId': user_id}
         return requests.get(url + "todos", params=params).json()
 
     with open('todo_all_employees.json', 'w') as file:
-        json.dump([{user['id'] : [{
+        json.dump([{user['id']: [{
             "username": user['username'],
             "task": todo['title'],
             "completed": todo['completed']} for todo in todos(user['id'])]
