f0389ee9f6ed53523a18bcb937e36db7ad4527dc
Kmag20 <owenmawira@gmail.com>
2024-05-08 21:22:37 +0300

    Web Stack debugging tres

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,5 @@
-exec { 'fix-wordpress':
+# Fix 500 error when a GET HTTP method is requested to Apache web server
+exec { 'fix-500err':
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
   path    => '/usr/local/bin/:/bin/'
 }
