dbe5815454771c2193aafe0990365ff71a17af0c
Kmag20 <owenmawira@gmail.com>
2024-05-08 21:25:28 +0300

    reddit api

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,33 +1,21 @@
 #!/usr/bin/python3
-
-"""Function to query subscribers on a given Reddit subreddit."""
+"""
+Script that queries subscribers on a given Reddit subreddit.
+"""
 
 import requests
-from urllib.parse import urljoin
-
-BASE_URL = "https://www.reddit.com"
-USER_AGENT = "Mozilla/5.0(by /u/CapillaryMercenary12)"
 
 
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
-    url = urljoin(BASE_URL, f"/r/{subreddit}/about.json")
-    headers = {"User-Agent": USER_AGENT}
-
-    try:
-        response = requests.get(url, headers=headers)
-        response.raise_for_status()
-    except requests.exceptions.HTTPError as err:
-        if response.status_code == 404:
-            return 0
-        else:
-            raise err
-
-    data = response.json().get("data")
-    return data.get("subscribers", 0)
-
-
-if __name__ == "__main__":
-    subreddit = input("Enter a subreddit name: ")
-    subscribers = number_of_subscribers(subreddit)
-    print(f"The subreddit /r/{subreddit} has {subscribers} subscribers.")
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        print("OK")
+        return subscribers
+    else:
+        print("OK")
+        return 0
